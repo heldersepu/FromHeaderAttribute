cddcfcd29b2072782213be25338d38d6b0c64f32
HelderSepu <HelderSepu@gmail.com>
2018-04-18 12:53:15 -0400

    Change the struct name

--- FromHeaderAttribute/Properties/AssemblyInfo.cs
+++ FromHeaderAttribute/Properties/AssemblyInfo.cs
@@ -1,23 +1,22 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle(Assemb.Title)]
+[assembly: AssemblyTitle(FromHeaderAttributeAssemb.Title)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RedRiverSoftware")]
-[assembly: AssemblyProduct(Assemb.Title)]
+[assembly: AssemblyProduct(FromHeaderAttributeAssemb.Title)]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("fd1d8798-d568-490f-aa1d-663930625c4e")]
 
-[assembly: AssemblyVersion(Assemb.Version)]
-[assembly: AssemblyFileVersion(Assemb.Version)]
-[assembly: AssemblyInformationalVersion(Assemb.Version)]
+[assembly: AssemblyVersion(FromHeaderAttributeAssemb.Version)]
+[assembly: AssemblyFileVersion(FromHeaderAttributeAssemb.Version)]
+[assembly: AssemblyInformationalVersion(FromHeaderAttributeAssemb.Version)]
 
-public struct Assemb
+public struct FromHeaderAttributeAssemb
 {
     public const string Title = @"FromHeaderAttribute";
     public const string Version = @"2.0.0.0";
