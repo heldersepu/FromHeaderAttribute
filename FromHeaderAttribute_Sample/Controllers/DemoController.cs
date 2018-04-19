4e9af9eb1dacd88e880cc78a6dc77e161b1adb1c
HelderSepu <HelderSepu@gmail.com>
2018-04-19 11:36:57 -0400

    Add one more action to demo

--- FromHeaderAttribute_Sample/Controllers/DemoController.cs
+++ FromHeaderAttribute_Sample/Controllers/DemoController.cs
@@ -5,8 +5,14 @@ namespace FromHeaderAttribute_Sample.Controllers
     [RoutePrefix("Demo")]
     public class DemoController : ApiController
     {
-        [Route("")]
+        [Route("Post")]
         public IHttpActionResult Post([FromHeader]string test)
+        {
+            return Ok(Request.Headers);
+        }
+
+        [Route("Put")]
+        public IHttpActionResult Put([FromHeader(Name = "abc")]string test)
         {
             return Ok(Request.Headers);
         }
